09583247af714d648e383d1c612fcbfb2c30962d
carrie57 <carrie91507@gmail.com>
2024-04-05 18:17:53 -0700

    feat: create update board function to update board after user hit

--- src/battleship/GUI.java
+++ src/battleship/GUI.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.effect.DropShadow;
@@ -18,6 +19,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -33,48 +35,45 @@ public class GUI extends Application {
 	private RadioButton aiRadioButton = new RadioButton("AI");
 	private RadioButton players2RadioButton = new RadioButton("2 Players");
 
+	// Create new board object
+	private Board board = new Board(true);
+
 	@Override
 	public void start(Stage primaryStage) {
-		// Set title "Battleship" style		
+		// Set title "Battleship" style
 		titleLabel.setFont(Font.font("Impact", 40));
 		titleLabel.setTextFill(Color.TRANSPARENT);
 
-        // Create a linear gradient for the text color
+		// Create a linear gradient for the text color
 		titleLabel.setTextFill(Color.rgb(0, 76, 153));
 
-        // Apply effects like drop shadow and reflection
+		// Apply effects like drop shadow and reflection
 		titleLabel.setEffect(new DropShadow());
 		titleLabel.setEffect(new Reflection());
-		
-		
+
 		// Create radio buttons for game mode selection
 		ToggleGroup gameModeGroup = new ToggleGroup();
 		aiRadioButton.setToggleGroup(gameModeGroup);
 		players2RadioButton.setToggleGroup(gameModeGroup);
-		aiRadioButton.setStyle("-fx-font-size: 16px;" +
-					"-fx-text-fill: rgb( 0, 0, 102);" +
-					"-fx-font-family: Roboto Slab");
-		players2RadioButton.setStyle("-fx-font-size: 16px;" +
-				"-fx-text-fill: rgb( 0, 0, 102);" +
-				"-fx-font-family: Roboto Slab");
+		aiRadioButton
+				.setStyle("-fx-font-size: 16px;" + "-fx-text-fill: rgb( 0, 0, 102);" + "-fx-font-family: Roboto Slab");
+		players2RadioButton
+				.setStyle("-fx-font-size: 16px;" + "-fx-text-fill: rgb( 0, 0, 102);" + "-fx-font-family: Roboto Slab");
 
 		// Create a button to start the game
 		Button startButton = new Button("S T A R T");
-		startButton.setStyle("-fx-background-color: rgb(0, 51, 102); " +
-                "-fx-text-fill: white; " +
-                "-fx-font-size: 16px; " +
-                "-fx-pref-width: 120px; " +
-                "-fx-pref-height: 40px;" +
-                "-fx-font-family: Impact");
+		startButton
+				.setStyle("-fx-background-color: rgb(0, 51, 102); " + "-fx-text-fill: white; " + "-fx-font-size: 16px; "
+						+ "-fx-pref-width: 120px; " + "-fx-pref-height: 40px;" + "-fx-font-family: Impact");
 		startButton.setOnAction(event -> {
 			if (aiRadioButton.isSelected()) {
 				// Start AI game
 				System.out.println("Starting AI game...");
-				startGame(primaryStage, true);
+				startGame(primaryStage, board, true);
 			} else if (players2RadioButton.isSelected()) {
 				// Start 2 players game
 				System.out.println("Starting 2 players game...");
-				startGame(primaryStage, false);
+				startGame(primaryStage, board, false);
 			} else {
 				// No game mode selected
 				System.out.println("Please select a game mode.");
@@ -98,59 +97,62 @@ public class GUI extends Application {
 		primaryStage.show();
 	}
 
-	public static void startGame(Stage primaryStage, boolean isAI) {
+	public static void startGame(Stage primaryStage, Board board, boolean isAI) {
 		// Create TextFields, inputs, and button as before
 
-		// Create TextFields for opponent and player's battlefield
-		TextField opponentField = new TextField();
-		opponentField.setEditable(false); // Set to read-only
-		opponentField.setPrefWidth(400); 
-        opponentField.setPrefHeight(400);
-		TextField playerField = new TextField();
-		playerField.setEditable(false); // Set to read-only
-		playerField.setPrefWidth(400);
-		playerField.setPrefHeight(400);
+		// Create TextArea for opponent and player's battlefield
+		TextArea opponentArea = new TextArea();
+//		opponentArea.setAlignment(Pos.CENTER);
+		opponentArea.setEditable(false); // Set to read-only
+		opponentArea.setMaxWidth(360);
+		opponentArea.setMaxHeight(368);
+		opponentArea.setWrapText(true);
+		opponentArea.setFont(Font.font("Courier New", FontWeight.BOLD, 27));
+
+		// Simulate making board and print board content
+		board.makeTest();
+		printBoard(board, opponentArea);
+
+		TextArea playerArea = new TextArea();
+		playerArea.setEditable(false); // Set to read-only
+		playerArea.setMaxWidth(360);
+		playerArea.setMaxHeight(368);
+		playerArea.setWrapText(true);
 
 		// Determine board labels based on isAI flag
-        Label leftLabel, rightLabel;
-        if (isAI) {
-            leftLabel = new Label("AI Board");
-            rightLabel = new Label("Your Board");
-        } else {
-            leftLabel = new Label("Player1 Board");
-            rightLabel = new Label("Player2 Board");
-        }
-        
-        // Set left label font and color	
-        leftLabel.setFont(Font.font("Georgia", FontWeight.BOLD, 20));
-        leftLabel.setTextFill(Color.rgb(0, 51, 102));
-        
-        // Set right label font and color	
-        rightLabel.setFont(Font.font("Georgia", FontWeight.BOLD, 20));
-        rightLabel.setTextFill(Color.rgb(0, 51, 102));
+		Label leftLabel, rightLabel;
+		if (isAI) {
+			leftLabel = new Label("AI Board");
+			rightLabel = new Label("Your Board");
+		} else {
+			leftLabel = new Label("Player1 Board");
+			rightLabel = new Label("Player2 Board");
+		}
 
-        // Create HBox for left and right labels
-        HBox labelBox = new HBox(300, leftLabel, rightLabel);
-        labelBox.setAlignment(Pos.CENTER);
-		
+		// Set left label font and color
+		leftLabel.setFont(Font.font("Georgia", FontWeight.BOLD, 20));
+		leftLabel.setTextFill(Color.rgb(0, 51, 102));
+
+		// Set right label font and color
+		rightLabel.setFont(Font.font("Georgia", FontWeight.BOLD, 20));
+		rightLabel.setTextFill(Color.rgb(0, 51, 102));
+
+		// Create HBox for left and right labels
+		HBox labelBox = new HBox(300, leftLabel, rightLabel);
+		labelBox.setAlignment(Pos.CENTER);
+
 		// Create input fields for x and y coordinates
 		TextField xInput = new TextField();
 		TextField yInput = new TextField();
 
 		// Create Guess button
 		Button guessButton = new Button("G u e s s");
-		guessButton.setStyle("-fx-background-color: rgb(0, 51, 102); " +
-                "-fx-text-fill: white; " +
-                "-fx-font-size: 14px; " +
-                "-fx-pref-width: 80px; " +
-                "-fx-pref-height: 32px;" +
-                "-fx-font-family: Impact");
+		guessButton
+				.setStyle("-fx-background-color: rgb(0, 51, 102); " + "-fx-text-fill: white; " + "-fx-font-size: 14px; "
+						+ "-fx-pref-width: 80px; " + "-fx-pref-height: 32px;" + "-fx-font-family: Impact");
 		guessButton.setOnAction(event -> {
-			// Process the guess based on xInput and yInput values
-			int x = Integer.parseInt(xInput.getText());
-			int y = Integer.parseInt(yInput.getText());
-			// Perform the game logic for the guess
-			System.out.println("Player guess: (" + x + ", " + y + ")");
+			// call userGuess() function to update board
+			updateBoard(board, opponentArea, xInput, yInput);
 		});
 
 		// Create a GridPane to hold the xInput, yInput, and guessButton
@@ -164,10 +166,10 @@ public class GUI extends Application {
 
 		// Create a BorderPane to hold all elements
 		BorderPane root = new BorderPane();
-		root.setPadding(new Insets(20));
+		root.setPadding(new Insets(40));
 		root.setTop(labelBox);
-		root.setLeft(opponentField);
-		root.setRight(playerField);
+		root.setLeft(opponentArea);
+		root.setRight(playerArea);
 		root.setBottom(inputGrid);
 
 		// Set up the scene
@@ -177,6 +179,38 @@ public class GUI extends Application {
 		primaryStage.setTitle("Battleship Game");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	private static void printBoard(Board b, TextArea textArea) {
+		char[][] testB = b.getCharBoard();
+		StringBuilder boardString = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				boardString.append(testB[i][j]).append(" ");
+				System.out.print(testB[i][j]);
+				System.out.print(" ");
+			}
+			boardString.append("\n");
+			System.out.println();
+		}
+		textArea.setText(boardString.toString());
+	}
+
+	private static void updateBoard(Board board, TextArea textArea, TextField xInput, TextField yInput) {
+		// Process the guess based on xInput and yInput values
+		int xValue = Integer.parseInt(xInput.getText());
+		int yValue = Integer.parseInt(yInput.getText());
+		
+		// Perform the game logic for the guess
+		System.out.println("Player guess: (" + xValue + ", " + yValue + ")");
+		
+		// if game still running update the board
+		if (board.getState()) {
+			board.guess(xValue, yValue);
+			printBoard(board, textArea);
+			xInput.clear();
+			yInput.clear();
+		}
 	}
 
 	public static void main(String[] args) {
